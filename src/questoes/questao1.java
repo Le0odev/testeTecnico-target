0f0a3f2df63f92fdabdded21ed3fdd0dfa560578
Le0odev <leonardovinicius09@hotmail.com>
2024-03-12 23:56:59 -0300

    Terceiro commit

---
+++ src/questoes/questao1.java
@@ -0,0 +1,22 @@
+package questoes;
+
+public class questao1 {
+
+	public static void main(String[] args) {
+		int indice = 13, k = 0, soma = 0;
+		
+		while (k < indice ) {
+			
+			
+			k = k + 1;
+			soma = soma + k;
+		}
+		
+		System.out.println("O resultado da soma é: " + soma);
+		
+		
+		
+		
+	}
+
+}
---
+++ src/questoes/questao4.java
@@ -0,0 +1,21 @@
+package questoes;
+
+public class questao4 {
+
+	public static void main(String[] args) {
+		//
+		
+		/* 
+		Ligo um interrupitor e deixo alguns minutos ligado e me dirigo pra uma sala,
+		verifico a temperatura da lampada.
+		Logo depois ligo outro e deixo ligado, e me dirijo pra sala que está ligada, 
+		automaticamente vou eliminar um da matematica e saber qual interrupitor liga
+		determinada lampada.
+		
+		
+		*/
+		//
+
+	}
+
+}
